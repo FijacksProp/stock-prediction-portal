1a843a732b284e8ce8799ebb3e10dcd9d21a2269
FijacksProp <fijacks2005@yahoo.com>
2025-09-19 11:17:29 +0100

    Authentication Functionalities

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
             <div className='p-5 text-center bg-light-dark rounded'>
                 <h1 className='text-light'>Stock Prediction Portal</h1>
                 <p className='text-light lead'>Stock prediction apps utilize algorithms to analyze historical stock data, market trends, and technical indicators to identify patterns and predict future price movements. Some apps incorporate sentiment analysis by examining news articles, social media posts, and other textual data to gauge market sentiment and its potential impact on stock prices.</p>
-                <Button text='Login' class='btn-outline-info' />
+                <Button text='Explore Now' class='btn-outline-info' url='/dashboard'/>
             </div>
       </div>
     </>
